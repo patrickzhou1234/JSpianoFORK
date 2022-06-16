c6dcd214933719750892f7816befea2d0ef57d07
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-06-15 21:41:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const curKey = document.querySelector(`.wkey[data-key="${e.keyCode}"],.bkey[data-key="${e.keyCode}"]`);
+
+  if (!audio) return; // stop the function from running altogether 
+  audio.currentTime = 0; // rewind to the start
+
+  audio.play();
+  curKey.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip if not transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.wkey, .bkey');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
